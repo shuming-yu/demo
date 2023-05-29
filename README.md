9aa69d715a6d4bb7170b5583ce1c79a1ea22ead2
Yu.Shu-Ming 余書銘 IEC1 <yu.shu-ming@inventec.com>
2023-05-29 18:13:41 +0800

    add vue-i18n

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 3. SwaggerHub
 - 參考 : https://app.swaggerhub.com/apis/MIKEQOO123/shumingyu/1.0.0
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+### 參考網址
+
+1. 在 Vue-cli 中使用 i18n 實作多國語系
+- 參考 : https://medium.com/easons-murmuring/%E5%9C%A8-vue-cli-%E4%B8%AD%E4%BD%BF%E7%94%A8-i18n-%E5%AF%A6%E4%BD%9C%E5%A4%9A%E5%9C%8B%E8%AA%9E%E7%B3%BB-720ec360783e
--- src/main.js
+++ src/main.js
@@ -1,5 +1,44 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueI18n  from 'vue-i18n'
 
-createApp(App).use(router).mount('#app')
+//import global json lang
+import en from './i18n/en-US.json'; // 存放英文翻譯
+import tw from './i18n/zh-TW.json'; // 存放繁體中文翻
+import cn from './i18n/zh-CN.json'; // 存放簡體中文翻
+
+//init setting
+Vue.config.productionTip = false;
+
+//init plugin
+Vue.use(VueI18n)
+Vue.use(router)
+
+//init 
+const i18n = getGlobalI18n();
+
+new Vue({
+  router,
+  i18n,
+  // store,
+  // vuetify,
+  render: h => h(App)
+}).$mount('#app');
+
+// createApp(App).use(router).mount('#app')
+
+
+/**
+ * 設定語系 i18n
+ */
+function getGlobalI18n() {
+  return new VueI18n({
+    locale: process.env.VUE_APP_Language, // 語言標識
+    messages: {
+      "en-US": en,
+      'zh-TW': tw,
+      'zh-CN': cn,
+    }
+  });
+}
